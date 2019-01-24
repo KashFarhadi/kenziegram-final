bb85497ad4b5bf5b886f6dd2245ff80c6c7d9ab2
Khashayar Farhadi <kashfarhadi@gmail.com>
2019-01-24 14:15:07 -0500

    heres all the good stuff with lots of comments

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 
 const app = express()
-const publicFolderPath = path.join(__dirname, "public")
+const publicFolderPath = path.join(__dirname, 'public')
 
 app.use(express.json())
 app.use(express.static(publicFolderPath))
 
-const users = []
+let users = []
 
 // add POST request listener here
+app.post('/api/user', (req, res) => {
+  // console.log(res.body) // this is a server side console, will show in terminal
 
-app.listen(3000);
+  let taken = false   // for the first time through, Set it to false
+      // because when the array is empty  it will skip through the for loop
+      // we want to push anything into the array since its impossible to have duplicates
+
+  for (user of users) {
+    if (user.username === req.body.username) {
+      taken = true
+      break
+    } else {
+      taken = false
+    }
+  }
+  if (taken) {
+    res.status(409).send()        
+    //.send() accepts an optional parameter which lets you specify the request's body; this is primarily used for requests such as PUT. If the request method is GET or HEAD, the body parameter is ignored and the request body is set to null
+    throw new Error ("Username is  Taken") 
+  } else {
+    req.body.id = Math.random() * (10 ^16)   // give random IDs to req.body
+    users.push(req.body)
+    res.status(201).send()
+    console.log(users)
+  }
+})
+
+let port = 3000     // so that we cna pass it into the app.listen function below
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
+// make sure you go to http://localhost:3000/ to see this page from the server
--- public/index.js
+++ public/index.js
@@ -1,2 +1,67 @@
-const userCreateForm = document.getElementById("user-create-form")
-const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+const userCreateForm = document.getElementById('user-create-form')
+const userCreateSubmitButton = userCreateForm.querySelector(
+  "button[type='submit']"
+)
+
+userCreateSubmitButton.addEventListener('click', function submitHander(event) {
+  event.preventDefault()
+
+  let userInput = document.getElementsByTagName('input')
+  let input = [] // this is just ONE WAY to combine all the inputs.
+  // [whole_name].value is for loop syntax, we use it.
+
+  for (let currentInput of userInput) {
+    console.log(currentInput)
+    console.log(currentInput.value)
+
+    input.push(currentInput.value)
+  }
+
+  // it doesn't grab this information from the form in HTML
+  // can make these whatever I want. You just made brand new variables.
+
+  let username = input[0]
+  let email = input[1]
+  let happyplace = input[2]
+  let firstpet = input[3]
+
+  let newUser = new User(username, email, happyplace, firstpet)
+  let formattedNewUser = JSON.stringify(newUser)
+
+  // stack overflow: https://stackoverflow.com/questions/39565706/post-request-with-fetch-api
+  fetch("/api/user/", {
+    // this is in the readme, receive post received at the pathin instructions
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    // we already stringified our json, otherwise we would do this with all our fields.
+    //  body: JSON.stringify({
+    //   name: myName,
+    //   email: myEmail
+    // etc etc
+    // })
+
+    body: formattedNewUser
+  }).then(response => {
+    //do something awesome that makes the world a better place
+    if (response.status === 201) {
+      alert('Success!')
+    } else if (response.status === 409) {
+      alert('Username is Taken fool!')
+    }
+  })
+  console.log('hi')
+})
+
+// MAKE NEW CLASSES IN A DIFFERENT FILE ON THE JOB
+
+class User {
+  constructor (username, email, happyplace, firstpet) {
+    this.username = username
+    this.email = email
+    this.happyplace = happyplace
+    this.firstpet = firstpet
+  }
+}
